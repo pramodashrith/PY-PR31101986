f23531464e13548d5fc1168e37e58ee6ab2b2896
Pramod Ashrith <pramodash86@gmail.com>
2023-08-16 11:52:43 +0530

    repo added

---
+++ Jenkins/core-backup/backup.py
@@ -0,0 +1,39 @@
+import os
+import requests
+import git
+
+# Jenkins API URL and credentials
+jenkins_url = 'https://your-jenkins-server'
+username = 'your-username'
+password = 'your-password'
+
+# Directory to store job configurations
+output_dir = 'job_configs'
+
+# Clone or open the Git repository
+repo_path = 'path/to/your/git/repo'
+repo = git.Repo(repo_path)
+
+# Get list of Jenkins jobs
+jobs_url = f'{jenkins_url}/api/json'
+response = requests.get(jobs_url, auth=(username, password))
+jobs = response.json()['jobs']
+
+# Iterate through jobs
+for job in jobs:
+    job_name = job['name']
+    job_url = f'{jenkins_url}/job/{job_name}/config.xml'
+    response = requests.get(job_url, auth=(username, password))
+    job_config = response.text
+
+    # Store job configuration as a file
+    job_filename = os.path.join(output_dir, f'{job_name}_job.xml')
+    with open(job_filename, 'w') as f:
+        f.write(job_config)
+
+    # Add and commit to Git repository
+    repo.git.add(job_filename)
+    repo.git.commit(m=f'Updated {job_name} job.xml')
+
+# Push changes to Git repository
+repo.git.push('origin', 'master')
